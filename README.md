9742a2caa073d0954092c76ffcc85e734ea52ffb
nfaltir <nfaltir@protonmail.com>
2021-10-28 21:12:05 -0700

    v2-cleaned code

---
+++ README.md
@@ -0,0 +1,35 @@
+Stock Comparison Dashboard
+===========================
+
+<br>
+
+Overview
+---------
+This is a simple streamlit web application that generates beautiful charts <br>
+using yfinance and matplotlib. No HTML, CSS, or JS needed. The streamlit <br>
+library will automatically build your python code into a web application, with some neat features. 
+
+
+
+
+<br>
+
+Features
+-----------
+1. compare multiple stocks
+2. generate charts (price, returns, volume)
+
+
+<br>
+
+Run locally
+-------------
+1. install packages `pip3 install -r requirements.txt`
+2. run app `streamlit run test.py`
+
+<br>
+
+<br><i>Output Figure</i>
+<img src="figure1.png"
+     alt="example-energy"
+     style="float: left; margin-right: 10px; margin-bottom: 50px;" />
--- test.py
+++ test.py
@@ -6,23 +6,30 @@ import seaborn as sns
 import numpy as np
 import yfinance as yf
 import csv
+
 st.markdown("<h1 style='text-align: center; color: #4C4C6D;'>Stock Comparison 🌱</h1>", unsafe_allow_html=True)
 st.markdown("<h4 style='text-align: center; font-weight: lighter; color: #FF8243; '><i><a style='text-decoration: none; color: #FF8243;' target='_blank' href='https://twitter.com/hoodie_coder'>Nodebanker ⚡️</a></i></h4>", unsafe_allow_html=True)
 st.write("-------------------------------------------------------- ")
+
+
 file = open('tickers.csv')
 tickers = (pd.read_csv(file))
-dropdown = st.multiselect("Pick Stock Ticker(s):", tickers)
+
+
+stock_options = st.multiselect("Pick Stock Ticker(s):", tickers)
 start = st.date_input('Select Start', value = pd.to_datetime('2020-01-01'))
 end = st.date_input('Select End', value = pd.to_datetime('today'))
-def relativeret(df):
+def relative_return(df):
     rel = df.pct_change()
     cumret = (1+rel).cumprod() - 1
     cumret = cumret.fillna(0)
     return cumret
-if len(dropdown) > 0:
-    closingPrice = yf.download(dropdown,start,end)['Adj Close']
-    stock_returns = relativeret(yf.download(dropdown,start,end)['Adj Close']) 
-    volume = yf.download(dropdown,start,end)['Volume']
+
+    
+if len(stock_options) > 0:
+    closingPrice = yf.download(stock_options,start,end)['Adj Close']
+    stock_returns = relative_return(yf.download(stock_options,start,end)['Adj Close']) 
+    volume = yf.download(stock_options,start,end)['Volume']
     st.write("### Closing Price")
     st.line_chart(closingPrice)
     st.write("### Returns")
